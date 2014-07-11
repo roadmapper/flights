1e6888a6bdaded3118146cf67bf35f64a9007827
Vinay Dandekar <vindansam@hotmail.com>
2014-07-11 16:40:09 -0400

    API building...

--- map_server/config/routes.rb
+++ map_server/config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  namespace :api do
+    namespace :v1 do
+     resources :flight
+    end
+  end
+  # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
