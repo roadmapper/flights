1b508327ae086adbca24d3f8a1e170e1a5778338
Vinay Dandekar <vindansam@hotmail.com>
2014-07-16 17:40:23 -0400

    Planes now show up on the map! InfoWindow on the map is messed up. Better proof of concept for a future Android app. The sprite logic is blatently ripped from flightaware24.com

--- map_server/app/models/flight.rb
+++ map_server/app/models/flight.rb
@@ -4,12 +4,19 @@ class Flight
   index(
     {location: "2d"},{min: -200, max: 200, background: true}
   )
-  field :altitude, type: Integer
-  field :speed, type: Integer
+  field :altitude_ft, type: Integer
+  field :speed_kts, type: Integer
   field :aircraft, type: String
   field :registration, type: String
   field :origin, type: String
   field :destination, type: String
   field :flightnum, type: String
   field :IATAflightnum, type: String
+
+  def as_json(options={})
+    attrs = super(options)
+    #attrs["id"] = attrs["_id"]
+    attrs
+  end
+
 end
