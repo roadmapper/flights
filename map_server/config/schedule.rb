8837c82a678c95f6442d6ce03fac2a51f2e940e9
Vinay Dandekar <vindansam@hotmail.com>
2014-07-21 15:06:46 -0400

    Changing scheduling and permssions for crontab

--- map_server/config/schedule.rb
+++ map_server/config/schedule.rb
@@ -1,4 +1,4 @@
-every 2.minutes do
+every 5.minutes do
   command "echo 'lol_haha ' >> /tmp/lol.log"
   #command "chromium-browser"
   command "/home/vinay/flights/updatedb.sh"
--- updatedb.sh
+++ updatedb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DB="map_server_development"
-mongo $DB --eval "printjson(db.flights.remove({}))"
-ruby flights.rb $DB
+mongo $DB --eval "printjson(db.flights.remove({})) " >> /tmp/updatedb.log
+ruby flights.rb $DB >> /tmp/updatedb.log
 echo "Loaded flights at: $(date)" >> /tmp/myflights.log
