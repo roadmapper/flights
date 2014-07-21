cbf10693a4bdcefc37f18e1ef1d225566ef093f4
Vinay Dandekar <vindansam@hotmail.com>
2014-07-21 15:09:30 -0400

    Path errors in updatedb.sh: need to be from root

--- updatedb.sh
+++ updatedb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DB="map_server_development"
 mongo $DB --eval "printjson(db.flights.remove({})) " >> /tmp/updatedb.log
-ruby flights.rb $DB >> /tmp/updatedb.log
+ruby /home/vinay/flights/flights.rb $DB >> /tmp/updatedb.log
 echo "Loaded flights at: $(date)" >> /tmp/myflights.log
