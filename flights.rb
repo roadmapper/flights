6ccd5068a874b1dfa9a829714d07772d86f2d387
Vinay Dandekar <vindansam@hotmail.com>
2014-07-29 11:39:05 -0400

    Added flight ID to the Flight model

--- flights.rb
+++ flights.rb
@@ -1,7 +1,6 @@
 require 'open-uri'
 require 'json/pure'
 require 'mongo'
-#require 'bson_ext'
 
 include Mongo
 
@@ -13,11 +12,9 @@ data = open("http://db8.flightradar24.com/zones/full_all.json").read
 data = JSON.parse(data)
 
 data.each do |d|
-	hash = {:hex => d[1][0], :location => [d[1][1], d[1][2]], :aircraft_track => d[1][3], :altitude_ft => d[1][4], :speed_kts => d[1][5], 
+	hash = {:flight_id => d[0], :hex => d[1][0], :location => [d[1][1], d[1][2]], :aircraft_track => d[1][3], :altitude_ft => d[1][4], :speed_kts => d[1][5], 
 		:squawk => d[1][6], :radar => d[1][7], :aircraft => d[1][8], :registration => d[1][9], :timestamp => d[1][10], :origin => d[1][11], 
 		:destination => d[1][12], :IATAflightnum => d[1][13], :ICAOflightnum => d[1][16]}
-	# jsonified = hash.to_json
-	#p hash
 	id = collection.insert(hash)
 	#p id
 end
--- map_server/app/models/flight.rb
+++ map_server/app/models/flight.rb
@@ -4,19 +4,13 @@ class Flight
   index(
     {location: "2d"},{min: -200, max: 200, background: true}
   )
+  field :flight_id, type: String
   field :altitude_ft, type: Integer
   field :speed_kts, type: Integer
   field :aircraft, type: String
   field :registration, type: String
   field :origin, type: String
   field :destination, type: String
-  field :flightnum, type: String
+  field :ICAOflightnum, type: String
   field :IATAflightnum, type: String
-
-  def as_json(options={})
-    attrs = super(options)
-    #attrs["id"] = attrs["_id"]
-    attrs
-  end
-
 end
